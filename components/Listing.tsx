53046f1108e062d39517cf100c71e710bc48d2a3
Cooper Saye <coopersaye@gmail.com>
2022-08-09 21:45:52 -0700

    Update listing props.

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -1,6 +1,24 @@
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import styles from '../styles/components/Listing.module.scss';
+import { ListingType } from '../util/listings';
 
-export default function Listing() {
+type Props = {
+  listing: ListingType
+};
+
+export default function Listing(props: Props) {
+  const { listing } = props;
+
+  // returns link to purchase given listing
+  function getBuyLink(listing: ListingType) {
+    const domain = listing.domain.toLowerCase();
+    if (listing.site === 'namecheap') {
+      return `https://www.namecheap.com/market/buynow/${domain}/`;
+    } else if (listing.site === 'dropcatch') {
+      return `https://www.dropcatch.com/domain/${domain}`;
+    }
+  }
+
   return (
     <div className={styles.container}>
     </div>
