1af7c3008acd630f5b6c54e1f2e17c7de7abc93d
Cooper Saye <coopersaye@gmail.com>
2022-08-09 21:47:11 -0700

    Update index page props.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,27 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import Listing from '../components/Listing';
 import styles from '../styles/pages/Index.module.scss';
+import { listings } from '../util/listings';
 
 export default function Index() {
+  const router = useRouter();
+
+  // highlight selected domain on start
+  useEffect(() => {
+    // get domain from router
+    const { d } = router.query;
+    if (typeof d !== 'string') return;
+    // get bookmark
+    const bookmark = document.getElementById(d.toLowerCase());
+    if (!bookmark) return;
+    // highlight listing
+    const listing = bookmark.parentElement;
+    if (!listing) return;
+    bookmark.scrollIntoView();
+    listing.className = `${styles.listing} ${styles.selected}`;
+  }, [router.query]);
+
   return (
     <div className={styles.container}>
     </div>
