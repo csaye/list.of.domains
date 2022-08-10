4f37630469126cd10e51f6c1c0d1a05c22ed307b
Cooper Saye <coopersaye@gmail.com>
2022-08-09 20:14:47 -0700

    Update favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,12 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>List of Domains</title>
-        <meta name="description" content="Unused domains for sale." />
-        <link rel="icon" href="/favicon.ico" />
+        <title>list.of.domains</title>
+        <meta name="description" content="Domains for sale." />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
