39ac1f15f3d662bc188b08e0236c7fbe5b54d6be
Cooper Saye <coopersaye@gmail.com>
2022-08-09 21:47:32 -0700

    Define listings.

---
+++ util/listings.ts
@@ -0,0 +1,148 @@
+export type ListingType = {
+  domain: string,
+  description: string,
+  site: 'namecheap' | 'dropcatch'
+};
+
+export const listings: ListingType[] = [
+  {
+    domain: 'BerkeleyEngineer.com',
+    description: 'personal portfolio website',
+    site: 'namecheap'
+  },
+  {
+    domain: 'Bicycle.social',
+    description: 'geo-based social network',
+    site: 'namecheap'
+  },
+  {
+    domain: 'Brainwave.team',
+    description: 'group idea development',
+    site: 'namecheap'
+  },
+  {
+    domain: 'BrainyCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'ChampionCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CloudDB.org',
+    description: 'cloud database services',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'CodeChampion.io',
+    description: 'code learning platform',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodeChampion.net',
+    description: 'code learning platform',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodeChampion.org',
+    description: 'code learning platform',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodersCreate.com',
+    description: 'coder collaboration forum',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodeTeach.org',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodeTeacher.org',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CodeWith.games',
+    description: 'code with games',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CometCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'CVXT.com',
+    description: 'cv resume builder',
+    site: 'namecheap'
+  },
+  {
+    domain: 'DevComms.io',
+    description: 'dev communications app',
+    site: 'namecheap'
+  },
+  {
+    domain: 'DirectMessage.org',
+    description: 'direct messaging service',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'Earlies.net',
+    description: 'early learning app for kids',
+    site: 'namecheap'
+  },
+  {
+    domain: 'Fields.fun',
+    description: 'field-themed online game',
+    site: 'namecheap'
+  },
+  {
+    domain: 'GiftedCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'MomDad.org',
+    description: 'parent organization',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'Somersault.net',
+    description: 'online gymnastics shop',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'TalentCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'TalentedCoding.com',
+    description: 'code tutoring service',
+    site: 'namecheap'
+  },
+  {
+    domain: 'TechApp.net',
+    description: 'tech services site',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'Unheed.com',
+    description: 'social movement site',
+    site: 'dropcatch'
+  },
+  {
+    domain: 'WickBridge.com',
+    description: 'online candle shop',
+    site: 'namecheap'
+  },
+  {
+    domain: 'WickFaces.com',
+    description: 'candle social network',
+    site: 'namecheap'
+  }
+];
